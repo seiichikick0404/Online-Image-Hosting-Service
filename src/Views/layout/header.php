d4550201dba06323b1f41fe21f08bf738ddfcf71
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-02-08 21:30:07 +0900

    ヘッダーとフッター作成

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -9,31 +9,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/css/style.css">
 
-    <title>Text Snippet Sharing Service</title>
+    <title>Online Image Hosting Service</title>
 </head>
 <body>
 
 <!-- Full-width Navbar Start -->
-<nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
-    <div class="container-fluid d-flex justify-content-center">
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Online-Image-Hosting-Service</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-center" id="navbarColor01">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="create">Home</a>
+                    <a class="nav-link" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="create">New Snippet</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="library">Snippet Library</a>
+                    <a class="nav-link" href="library">Image Library</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
+
 
 
 
