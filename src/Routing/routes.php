361f438ccd24b00ddca429236a6bac59b7369787
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-02-15 21:45:57 +0900

    不要なルーティング削除

--- src/Routing/routes.php
+++ src/Routing/routes.php
@@ -11,13 +11,6 @@ use Response\Render\JSONRenderer;
 
 
 return [
-    'snippet/create'=>function(): HTTPRenderer{
-        $syntaxes = DatabaseHelper::getSyntaxes();
-        $errors = isset($_SESSION['errors']) ? $_SESSION['errors'] : null;
-        unset($_SESSION['errors']);
-
-        return new HTMLRenderer('component/createSnippet', ['syntaxes' => $syntaxes, 'errors' => $errors]);
-    },
     'snippet/save'=>function(): HTTPRenderer{
         $errors = ValidationHelper::createSnippetPost($_POST);
         if (count($errors) > 0) {
@@ -31,23 +24,6 @@ return [
 
         return new HTMLRenderer('component/show');
    },
-    'snippet/show'=>function(): HTTPRenderer{
-        $path = $_GET['uniqueKey'] ?? null;
-
-        if (!$path) {
-            header("Location: create");
-            exit;
-        }
-        $path = ValidationHelper::string($path);
-        $data = DatabaseHelper::getSnippet($path);
-
-        return new HTMLRenderer('component/show', ['data' => $data]);
-   },
-    'api/parts'=>function(){
-        $id = ValidationHelper::integer($_GET['id']??null);
-        $part = DatabaseHelper::getComputerPartById($id);
-        return new JSONRenderer(['part'=>$part]);
-    },
     'top'=>function(){
         return new HTMLRenderer('component/createImage');
     },
@@ -64,6 +40,6 @@ return [
         //ここでデータを保存 
         $responseData = DatabaseHelper::createImage($title, $image, $clientIp);
 
-        return new JSONRenderer(["response" => $responseData]);
+        return new JSONRenderer(["response" => $image]);
     },
 ];
